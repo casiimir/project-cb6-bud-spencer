7d8b0b7976303989290e749b7cd62b723b133df6
alekyari <1alessio.perez@gmail.com>
2023-04-11 18:28:15 +0200

    11-04-2023 second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <h1>Budz</h1>
       </main>
     </>
   )
