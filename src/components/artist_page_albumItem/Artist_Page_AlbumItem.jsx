0207812ab20fb951268428714dced8fa8bffb19d
Luisazizzo <luisa0zizzo@gmail.com>
2023-04-14 18:22:43 +0200

    css artit_page

--- src/components/artist_page_albumItem/Artist_Page_AlbumItem.jsx
+++ src/components/artist_page_albumItem/Artist_Page_AlbumItem.jsx
@@ -4,18 +4,17 @@ import Image from "next/image";
 const Artist_Page_AlbumItem = ({ data }) => {
   return (
     <div className={styles.container}>
-            <Image
-              src={data.cover_small}
-              alt={data.title}
-              width={120}
-              height={120}
-            />
-            <div className={styles.text}>
-            <h2>{data.title}</h2>
-            <p>{`Released on ${data.release_date}`}</p>
-            </div>
-          </div>
-   
+      <Image
+        src={data.cover_medium}
+        alt={data.title}
+        width={120}
+        height={120}
+      />
+      <div className={styles.text}>
+        <h4>{data.title}</h4>
+        <p>{`Released on ${data.release_date}`}</p>
+      </div>
+    </div>
   );
 };
 
--- src/components/artist_page_trackItem/Artist_Page_TrackItem.jsx
+++ src/components/artist_page_trackItem/Artist_Page_TrackItem.jsx
@@ -4,18 +4,17 @@ import Image from "next/image";
 const Artist_Page_TrackItem = ({ data }) => {
   return (
     <div className={styles.container}>
-            <Image
-              src={data.album.cover_small}
-              alt={data.title}
-              width={120}
-              height={120}
-            />
-           <div className={styles.text}>
-            <h2>{data.title}</h2>
-            <p>{`Album: ${data.album.title}`}</p>
-          </div>
+      <Image
+        src={data.album.cover_medium}
+        alt={data.title}
+        width={120}
+        height={120}
+      />
+      <div className={styles.text}>
+        <h4>{data.title}</h4>
+        <p>{`Album: ${data.album.title}`}</p>
       </div>
- 
+    </div>
   );
 };
 
