5b3411e3123d56b63bff199167c8576432c9aba4
Luisazizzo <luisa0zizzo@gmail.com>
2023-04-15 21:43:23 +0200

    Aggiunta search_page

--- src/components/artist_page_artistItem/Artist_Page_ArtistItem.jsx
+++ src/components/artist_page_artistItem/Artist_Page_ArtistItem.jsx
@@ -4,17 +4,19 @@ import Image from "next/image";
 const Artist_Page_ArtistItem = ({ artistData }) => {
   return (
     <div className={styles.container}>
-      <Image
-        className={styles.img}
-        src={artistData.picture_medium}
-        width={200}
-        height={200}
-        alt={artistData.name}
-      />
-      <div className={styles.text}>
-        <h1>{artistData.name}</h1>
+      <div className={styles.info_artist}>
+        <Image
+          className={styles.img}
+          src={artistData.picture_medium}
+          width={200}
+          height={200}
+          alt={artistData.name}
+        />
         <p>{`N° Albums: ${artistData.nb_album}`}</p>
         <p>{`Follower: ${artistData.nb_fan}`}</p>
+      </div>
+      <div className={styles.name_artist}>
+        <h1>{artistData.name}</h1>
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import { HiMenuAlt1, HiOutlineBell } from "react-icons/hi";
 import { BiSearchAlt } from "react-icons/bi";
 import styles from "./index.module.scss";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Navbar = ({ title }) => {
+  const [inputSearch, setInputSearch] = useState("");
+  const router = useRouter();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (inputSearch.trim()) {
+      router.push(`/search_page/${inputSearch.trim()}`);
+      setInputSearch("");
+    }
+  };
   return (
     <div className={styles.Navbar}>
       <h1 className={styles.title_home}>{title}</h1>
@@ -12,7 +24,14 @@ const Navbar = ({ title }) => {
       <div className={styles.content_icon_input}>
         <HiOutlineBell className={styles.icon_bell} />
         <BiSearchAlt className={styles.icon_search} />
-        <input type="text" placeholder="Search" />
+        <form onSubmit={onSubmit}>
+          <input
+            value={inputSearch}
+            onChange={(e) => setInputSearch(e.target.value)}
+            type="text"
+            placeholder="Search"
+          />
+        </form>
       </div>
     </div>
   );
