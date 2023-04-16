b2d8625fe656e0e8deaf1b9d8020035ae5c298ac
Luisazizzo <luisa0zizzo@gmail.com>
2023-04-16 11:43:12 +0200

    Aggiunta metoto

--- src/utils/func.js
+++ src/utils/func.js
@@ -3,18 +3,18 @@
 //     return sec;
 //   };
 
-  function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
+export const shortDescription = (str) =>
+  str.split(" ").splice(0, 3).join(" ") + "...";
 
-  const secondsToMinutes = (totalSeconds) => {
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-    const result = `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
-    return result;
-  };
+function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
+}
 
+const secondsToMinutes = (totalSeconds) => {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  const result = `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}`;
+  return result;
+};
 
-  export {secondsToMinutes,padTo2Digits}
-
-
+export { secondsToMinutes, padTo2Digits };
