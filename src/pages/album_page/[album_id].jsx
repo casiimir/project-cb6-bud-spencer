1bf3d5d65ea1619fdc65cb55f3e38dbcb2fd2fff
alekyari <1alessio.perez@gmail.com>
2023-04-18 14:00:40 +0200

    track page e barra in album page

--- src/pages/album_page/[album_id].jsx
+++ src/pages/album_page/[album_id].jsx
@@ -8,9 +8,15 @@ import { useState } from "react";
 export default function Search_page({ albumData }) {
   const [player, setPlayer] = useState(false);
 
-  const onHandleShowPlayer = () =>{
-    setPlayer(true)
-  }
+  const onHandleShowPlayer = () => {
+    setPlayer(true);
+  };
+
+  
+
+  const onHandleHidePlayer = () => {
+    setPlayer(false);
+  };
   return (
     <MainLayout>
       <div
@@ -19,28 +25,33 @@ export default function Search_page({ albumData }) {
       >
         <div className={styles.container}>
           <Navbar title={"Album"} />
-          {player && <div className={styles.deezerPlayer}>
-          <iframe 
-            title="deezer-widget"
-            src={`https://widget.deezer.com/widget/dark/album/${albumData.id}?tracklist=false`}
-            width="100%"
-            height="130"
-            frameborder="0"
-            allowtransparency="true"
-            allow="encrypted-media; clipboard-write"
-          ></iframe>
-          </div>}
+          {player && (
+            <div className={styles.deezerPlayer}>
+              <iframe
+                title="deezer-widget"
+                src={`https://widget.deezer.com/widget/dark/album/${albumData.id}?tracklist=false`}
+                width="100%"
+                height="130"
+                frameborder="0"
+                allowtransparency="true"
+                allow="encrypted-media; clipboard-write"
+              ></iframe>
+              <div className={styles.closePlayer}   onClick={onHandleHidePlayer}>x</div>
+            </div>
+          )}
           <div className={styles.albumItem}>
             <Album_Page_AlbumItem data={albumData} />
           </div>
-          <h2 style={{ padding: `20px` }}  >Tracks </h2> 
-          <h3 style={{ padding: `20px` }} onClick={onHandleShowPlayer} >Listen now!</h3>
-          <div className={styles.trackItem} >
+          <div className={styles.titles}>
+            <h2>Tracks </h2>
+            <h3 className={styles.buttonPlayer} onClick={onHandleShowPlayer}>Listen now all the tracks!</h3>
+          </div>
+
+          <div className={styles.trackItem}>
             {albumData?.tracks.data.map((data, i) => (
-              <Home_Page_TrackItem key={i} data={data} trackIndex={i}   />
+              <Home_Page_TrackItem key={i} data={data} trackIndex={i} />
             ))}
           </div>
-          
         </div>
       </div>
     </MainLayout>
